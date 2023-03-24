5347c2ebdb5824a45ea4a609d3e6fcabcf3d63b9
denniwind <denniwind@live.dk>
2023-03-24 11:57:19 +0100

    Needed files : Added * Added needed classes

---
+++ GDPRManager/CommandPattern/PauseCommand.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GDPRManager.ComponentPattern;
+
+namespace GDPRManager.CommandPattern
+{
+    /// <summary>
+    /// class for the movecommand
+    /// </summary>
+    public class PauseCommand : ICommand
+    {
+        private Vector2 velocity;
+
+        /// <summary>
+        /// sets the velocity based on the input
+        /// </summary>
+        /// <param name="velocity">which direction we want</param>
+        public PauseCommand(Vector2 velocity)
+        {
+            this.velocity = velocity;
+        }
+
+        /// <summary>
+        /// calls the move command on the player
+        /// </summary>
+        /// <param name="player">the player we need to execute the method on</param>
+        public void Execute(Player player)
+        {
+            player.Move(velocity);
+        }
+    }
+}
