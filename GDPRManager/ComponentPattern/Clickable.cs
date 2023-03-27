42c97a7483293360af246073519618498348d71a
IdaMarcherJensen <54858@student.eadania.dk>
2023-03-27 13:27:16 +0200

    ComponentPattern : Modified * added MousePointer * added Clickable * modfied CaseStack

---
+++ GDPRManager/ComponentPattern/Clickable.cs
@@ -0,0 +1,16 @@
+﻿using GDPRManager.CommandPattern;
+using GDPRManager.ObserverPattern;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GDPRManager.ComponentPattern
+{
+    public class Clickable : Component
+    {
+    }
+}
