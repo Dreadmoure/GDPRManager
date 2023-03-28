ebccf63462fb6a80121a4a1f043b1992aaa0d97c
denniwind <denniwind@live.dk>
2023-03-28 11:21:08 +0200

    ButtonBuilders : Added * Added builders for buttons

---
+++ GDPRManager/BuilderPattern/ApproveButtonBuilder.cs
@@ -0,0 +1,34 @@
+﻿using GDPRManager.ComponentPattern;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GDPRManager.BuilderPattern
+{
+    public class ApproveButtonBuilder : IBuilder
+    {
+        private GameObject gameObject;
+
+        public void BuildGameObject()
+        {
+            gameObject = new GameObject();
+
+            BuildComponents();
+        }
+
+        private void BuildComponents()
+        {
+            ApproveButton approveButton = (ApproveButton)gameObject.AddComponent(new ApproveButton());
+            gameObject.AddComponent(new SpriteRenderer());
+            gameObject.AddComponent(new Collider());
+            gameObject.AddComponent(new Clickable());
+        }
+
+        public GameObject GetResult()
+        {
+            return gameObject;
+        }
+    }
+}
