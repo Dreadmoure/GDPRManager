b344e8ec96d86e18505a2d6732ea24dd4d904ea0
denniwind <denniwind@live.dk>
2023-03-31 14:02:28 +0200

    StickyNote.cs : Modified * Changed scale

--- GDPRManager/ComponentPattern/StickyNote.cs
+++ GDPRManager/ComponentPattern/StickyNote.cs
@@ -20,7 +20,7 @@ namespace GDPRManager.ComponentPattern
             SpriteRenderer spriteRenderer = GameObject.GetComponent<SpriteRenderer>() as SpriteRenderer;
             spriteRenderer.SetSprite("Sprites\\StickyNote");
             spriteRenderer.LayerDepth = 0.5f;
-            spriteRenderer.Scale = 0.7f;
+            spriteRenderer.Scale = 1f;
             GameObject.Transform.Position = new Vector2(GameWorld.ScreenSize.X / 1.2f, GameWorld.ScreenSize.Y / 3f);
             GameObject.Tag = "StickyNote";
             Text = "";
