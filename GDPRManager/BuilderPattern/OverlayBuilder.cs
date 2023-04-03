2be64576f12343a9ee0410629501f6655f8b235c
denniwind <denniwind@live.dk>
2023-04-03 12:41:41 +0200

    Overlay : Added * Added overlay component og builder

---
+++ GDPRManager/BuilderPattern/OverlayBuilder.cs
@@ -0,0 +1,32 @@
+﻿using GDPRManager.ComponentPattern;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GDPRManager.BuilderPattern
+{
+    public class OverlayBuilder : IBuilder
+    {
+        private GameObject gameObject;
+
+        public void BuildGameObject()
+        {
+            gameObject = new GameObject();
+
+            BuildComponents();
+        }
+
+        private void BuildComponents()
+        {
+            Overlay overlay = (Overlay)gameObject.AddComponent(new Overlay());
+            gameObject.AddComponent(new SpriteRenderer());
+        }
+
+        public GameObject GetResult()
+        {
+            return gameObject;
+        }
+    }
+}
---
+++ GDPRManager/ComponentPattern/Overlay.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GDPRManager.ComponentPattern
+{
+    public class Overlay : Component
+    {
+        /// <summary>
+        /// sets sprite and position
+        /// </summary>
+        public override void Start()
+        {
+            SpriteRenderer spriteRenderer = GameObject.GetComponent<SpriteRenderer>() as SpriteRenderer;
+            spriteRenderer.SetSprite("Sprites\\BackgroundV2");
+            spriteRenderer.LayerDepth = 0.1f;
+            spriteRenderer.Scale = 1f;
+            GameObject.Transform.Position = new Vector2(GameWorld.ScreenSize.X / 2, GameWorld.ScreenSize.Y / 2);
+        }
+    }
+}
