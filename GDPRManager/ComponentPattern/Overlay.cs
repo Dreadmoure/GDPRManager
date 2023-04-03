3db3d421af8d9b50b6a915a606514ef505aec34e
denniwind <denniwind@live.dk>
2023-04-03 12:42:27 +0200

    Overlay.cs : Modified * Renamed

--- GDPRManager/ComponentPattern/Overlay.cs
+++ GDPRManager/ComponentPattern/Overlay.cs
@@ -14,7 +14,7 @@ namespace GDPRManager.ComponentPattern
         public override void Start()
         {
             SpriteRenderer spriteRenderer = GameObject.GetComponent<SpriteRenderer>() as SpriteRenderer;
-            spriteRenderer.SetSprite("Sprites\\BackgroundV2");
+            spriteRenderer.SetSprite("Sprites\\Overlay");
             spriteRenderer.LayerDepth = 0.1f;
             spriteRenderer.Scale = 1f;
             GameObject.Transform.Position = new Vector2(GameWorld.ScreenSize.X / 2, GameWorld.ScreenSize.Y / 2);
